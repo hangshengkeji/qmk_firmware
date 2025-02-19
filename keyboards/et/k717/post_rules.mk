d36268f221b4cd61657b9023a47aba0e0e45e1c8
yangzheng <1345509173@q.com>
2025-02-19 09:51:10 +0800

    Update the dynamic lighting effect settings of K717 and change the wireless path.

--- keyboards/et/k717/post_rules.mk
+++ keyboards/et/k717/post_rules.mk
@@ -2,4 +2,4 @@ RGB_MATRIX_CUSTOM_USER = yes
 RGBLIGHT_DRIVER = custom
 include keyboards/et/k717/rgb_record/rgb_record.mk
 include keyboards/et/k717/wls/wls.mk
-include keyboards/et/wireless.mk
+include keyboards/et/wireless/wireless.mk
